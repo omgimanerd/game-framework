180b9ad803464e39f71cdf9ddda633e3e234c3a3
omgimanerd <alvin.lin.dev@gmail.com>
2017-02-21 21:10:33 -0500

    Updated Sound.js

--- public/js/game/Sound.js
+++ public/js/game/Sound.js
@@ -17,39 +17,33 @@ Sound.BASE_URL = '/public/sound/';
 /**
  * Example:
  * Sound.SOUND_SRCS = {
- *   explosion: [
- *     'explosion1.mp3',
- *     'explosion2.mp3',
- *   ],
- *   gun: [
- *      'gun.mp3'
- *   ]
+ *   explosion: 'explosion1.mp3',
+ *   gun: 'gun.mp3'
  * }
  */
 Sound.SOUND_SRCS = {};
 
 /**
- * Factory method for creating a Sound object.
+ * Factory method for creating a Sound object. This initializes all the Howl
+ * objects and passes them into the Sound object instance.
  * @return {Sound}
  */
 Sound.create = function() {
-  var sounds = {};
-  for (var key in Sound.SOUND_SRCS) {
-    sounds[key] = Sound.SOUND_SRCS[key].map((src) => {
-      return new Howl({ src: Sound.BASE_URL + src });
-    });
-  }
-  return new Sound(sounds);
+  return new Sound(Sound.SOUND_SRCS.map((src) => {
+    return new Howl({ src: Sound.BASE_URL + src });
+  }));
 };
 
 /**
  * Plays a sound.
- * @param {string} sound The name of the sound to play, as defined in SOUND_SRCS,
- * @param {?number=} volume The volume to play the sound at, a number from 0 to 1.
+ * @param {string} sound The name of the sound to play, as defined in
+ *   SOUND_SRCS,
+ * @param {?number=} volume The volume to play the sound at, a number
+ *   from 0 to 1.
  * @param {?number=} rate The playback rate, a number from 0.5 to 4.
  */
 Sound.prototype.play = function(sound, volume, rate) {
-  var sound = Util.choiceArray(this.sounds[sound]);
+  var sound = this.sounds[sound];
   var id = sound.play();
   if (typeof(volume) === 'number') {
     sound.volume(Util.bound(volume, 0, 1), id);
